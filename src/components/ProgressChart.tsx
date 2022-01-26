ae70cb21cbc05bdef91482647b7776ef7a367893
Zonnaslyric <lovelyjhames@gmail.com>
2022-01-26 15:45:09 +0100

    progress chart

--- src/components/ProgressChart.tsx
+++ src/components/ProgressChart.tsx
@@ -8,19 +8,27 @@ export default function ProgressChart() {
   ];
 
   return (
-     <div className="w-full h-full flex justify-content-center">
-      <div className=" w-5/6 m-auto flex  py-3 text-white text-sm font-bold font-mono leading-6 border-x-2 border-x-gray-500 border-dashed divide-x  divide-dashed">
+    <div className="w-full h-full flex flex-col justify-content-center">
+      <div className="w-5/6 m-auto flex  py-3 text-white text-sm font-bold font-mono leading-6 border-x-2 border-x-gray-500 border-dashed divide-x  divide-dashed">
         {progressItem.map((data, index) => (
           <div
             className={`tooltip flex-initial p-6 flex items-center justify-center shadow-lg inner__0${index}`}
             key={index}
-            style={{ backgroundColor: data.backgroundColor, width:data.width}}
+            style={{ backgroundColor: data.backgroundColor, width: data.width }}
           >
-           <span className="tooltiptext">{data.width}</span>
+            <span className="tooltiptext">{data.width}</span>
           </div>
         ))}
       </div>
-
+      <div className="flex text-xs mx-auto w-5/6 defaultSectioning">
+        <div className="flex flex-1 w-full">0</div>
+        <div className="flex flex-1 w-full">20</div>
+        <div className="flex flex-1 w-full">40</div>
+        <div className="flex flex-1 w-full">60</div>
+        <div className="flex flex-1 w-full">80</div>
+        <div className="flex flex-1 w-full justify-content-end">
+          <span>100</span> </div>
+      </div>
     </div>
   );
 }
